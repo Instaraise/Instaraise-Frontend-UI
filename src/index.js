8b2fc28911841ae5a9e46cfd675b6deeb861252b
Anish <anishtiwari12345@gmail.com>
2022-09-07 19:02:31 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomeContainer from './container/HOME/HomeContainer';
+import Root from './routes/root';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <HomeContainer />
+        <Root />
     </React.StrictMode>
 );
---
+++ src/routes/root.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HomeContainer from '../container/HOME/HomeContainer';
+const Root = () => {
+    return <HomeContainer />;
+};
+
+export default Root;
