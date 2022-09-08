a08f4aa8e5add587085959524822e461151343cd
Anish <anishtiwari12345@gmail.com>
2022-09-08 17:50:01 +0530

    changes

--- src/components/home/LandingPage/Body/body.js
+++ src/components/home/LandingPage/Body/body.js
@@ -7,12 +7,37 @@ import twitterLogo from '../../../../assets/images/socials/twitter.svg';
 import upRight from '../../../../assets/images/arrow-up-right.svg';
 import '../../../../scss/_body.css';
 const Body = () => {
+    var HomePageDesElem = (
+        <>
+            {DESC_DATA.map((elem) => (
+                <>
+                    <div className="mt-5">
+                        <p className="mb-0 homepage-body-description-bodypara">
+                            {elem.heading}
+                        </p>
+                    </div>
+                    <div className="mb-4">
+                        <h1>{elem.subHeading}</h1>
+                    </div>
+                    <div className="vlb">
+                        <p className="ml-2">{elem.para}</p>
+                    </div>
+                </>
+            ))}
+            <button type="button" className=" btn btn-light btn-lg mt-4">
+                <span>
+                    Launch App
+                    <img className="ml-2" src={upRight} />
+                </span>
+            </button>
+        </>
+    );
     return (
         <section className="homepage-body">
             <Header />
             <div className="container">
                 <div className="row">
-                    <div className="col-2 col-md-2">
+                    <div className="col-2 col-md-2 d-none d-lg-block">
                         <div className="vl"></div>
                         <div className="homepage-body-socials">
                             <img src={instagramLogo} />
@@ -23,31 +48,11 @@ const Body = () => {
                         </div>
                         <div className="vl"></div>
                     </div>
-                    <div className="col-7 col-md-7 mx-auto homepage-body-description">
-                        {DESC_DATA.map((elem) => (
-                            <>
-                                <div className="mt-5">
-                                    <p className="mb-0 homepage-body-description-headerpara">
-                                        {elem.heading}
-                                    </p>
-                                </div>
-                                <div className="mb-4">
-                                    <h1>{elem.subHeading}</h1>
-                                </div>
-                                <div className="vlb">
-                                    <p className="ml-2">{elem.para}</p>
-                                </div>
-                            </>
-                        ))}
-                        <button
-                            type="button"
-                            className=" btn btn-light btn-lg mt-4"
-                        >
-                            <span>
-                                Launch App
-                                <img className="ml-2" src={upRight} />
-                            </span>
-                        </button>
+                    <div className="col-7 col-md-7 mx-auto homepage-body-description d-none d-lg-block">
+                        {HomePageDesElem}
+                    </div>
+                    <div class="col-md-12 mx-auto homepage-body-description d-lg-none">
+                        {HomePageDesElem}
                     </div>
                 </div>
             </div>
--- src/components/home/LandingPage/Stats/stats.js
+++ src/components/home/LandingPage/Stats/stats.js
@@ -3,12 +3,12 @@ import { PRICE_DATA } from '../../../../config/HomeConfig/StatsConfig/config.sta
 import '../../../../scss/_stats.css';
 const Stats = () => {
     return (
-        <section className=" two-background ">
+        <section className=" two-background mb-5">
             <div className="container ">
                 <div className="details">
                     <div className="row">
                         {PRICE_DATA.map((elem) => (
-                            <div className="col">
+                            <div className="col my-4">
                                 <p>{elem.name}</p>
                                 <h4>{elem.amount}</h4>
                             </div>
