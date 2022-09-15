cb756a059fd7ea845867e0754a8924d45231d6e1
Anish <anishtiwari12345@gmail.com>
2022-09-15 17:25:25 +0530

    changes

--- src/components/home/Footer/footer.js
+++ src/components/home/Footer/footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import instaLogo from '../../../assets/images/InstaLogo.svg';
 import CompName from '../../../assets/images/Instaraise.svg';
-import instagramLogo from '../../../assets/images/socials/instagram.svg';
-import linkedinLogo from '../../../assets/images/socials/linkedin.svg';
-import twitterLogo from '../../../assets/images/socials/twitter.svg';
+// import instagramLogo from '../../../assets/images/socials/instagram.svg';
+// import linkedinLogo from '../../../assets/images/socials/linkedin.svg';
+// import twitterLogo from '../../../assets/images/socials/twitter.svg';
 import { FOOTER_DATA } from '../../../config/HomeConfig/FooterConfig/config.footer';
+import { SOCIALS_DATA } from '../../../config/HomeConfig/FooterConfig/config.footer';
 const Footer = () => {
     return (
         <footer className="footer-section">
@@ -26,18 +26,24 @@ const Footer = () => {
                             Contact Us:
                         </h4>
                         <div className="my-2">
-                            <img
-                                className="cursor-pointer"
-                                src={instagramLogo}
-                            />
-                            <img
-                                className="ml-2 cursor-pointer"
-                                src={linkedinLogo}
-                            />
-                            <img
-                                className="ml-2 cursor-pointer"
-                                src={twitterLogo}
-                            />
+                            {SOCIALS_DATA.map((elem, index) => (
+                                <Link
+                                    key={index}
+                                    to="/"
+                                    target="_blank"
+                                    onClick={() =>
+                                        window.open(
+                                            elem.footer_social_link,
+                                            '_blank'
+                                        )
+                                    }
+                                >
+                                    <img
+                                        className="mx-2"
+                                        src={elem.footer_social_img}
+                                    />
+                                </Link>
+                            ))}
                         </div>
                     </div>
                     <div className=" col-lg-6 col-md-6 mb-4 mb-md-0 d-flex justify-content-start justify-content-lg-end justify-content-md-end">
--- src/config/HomeConfig/FooterConfig/config.footer.js
+++ src/config/HomeConfig/FooterConfig/config.footer.js
@@ -1,3 +1,6 @@
+import instagramLogo from '../../../assets/images/socials/instagram.svg';
+import linkedinLogo from '../../../assets/images/socials/linkedin.svg';
+import twitterLogo from '../../../assets/images/socials/twitter.svg';
 export const FOOTER_DATA = [
     {
         id: 1,
@@ -14,5 +17,24 @@ export const FOOTER_DATA = [
     {
         id: 4,
         footer_links: 'Privacy Policy',
+    },
+];
+export const SOCIALS_DATA = [
+    {
+        id: 1,
+        footer_social_img: instagramLogo,
+        footer_social_link: 'https://www.instagram.com/instaraise.io/',
+    },
+    {
+        id: 2,
+        footer_social_img: linkedinLogo,
+        footer_social_link:
+            'https://www.linkedin.com/company/instaraise/?originalSubdomain=in',
+    },
+    {
+        id: 3,
+        footer_social_img: twitterLogo,
+        footer_social_link:
+            'https://twitter.com/Instaraise?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
     },
 ];
