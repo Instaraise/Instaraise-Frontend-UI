f11dd0e1137f600770a3b02c1167ff3163cc2b10
Anish <anishtiwari12345@gmail.com>
2022-09-17 17:52:07 +0530

    backers

--- src/components/home/Backers/backers.js
+++ src/components/home/Backers/backers.js
@@ -1,86 +1,32 @@
 import React from 'react';
-import space_farm_img from '../../../assets/images/space_farm_img.svg';
-import mexc_img from '../../../assets/images/mexc_img.svg';
+import { BACKERS_DATA } from '../../../config/HomeConfig/BackersConfig/config.backers';
 const Backers = () => {
     return (
         <div className="container">
-            <div
-                id="carouselExampleDark"
-                className="carousel carousel-dark slide"
-                data-bs-ride="carousel"
-            >
-                <div className="carousel-indicators">
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleDark"
-                        data-bs-slide-to="0"
-                        className="active"
-                        aria-current="true"
-                        aria-label="Slide 1"
-                    ></button>
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleDark"
-                        data-bs-slide-to="1"
-                        aria-label="Slide 2"
-                    ></button>
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleDark"
-                        data-bs-slide-to="2"
-                        aria-label="Slide 3"
-                    ></button>
-                </div>
-                <div className="carousel-inner">
-                    <div
-                        className="carousel-item active"
-                        data-bs-interval="10000"
-                    >
-                        <img
-                            src={space_farm_img}
-                            className="d-block w-100"
-                            alt="..."
-                            height="100"
-                            width="100"
-                        />
+            <div class="d-flex pt-3 pb-4 justify-content-center">
+                <h2 class="fs-1 text-aileron-bold text-insta-regular">
+                    &nbsp;Our
+                </h2>
+                <h2 class="fs-1 text-aileron-bold services-text">
+                    &nbsp;Backers
+                </h2>
+            </div>
+
+            <div className=" row row-cols-2 row-cols-lg-5 row-cols-md-5  mx-0 mb-5">
+                {BACKERS_DATA.map((elem) => (
+                    <div className="col-6 col-md-2 d-flex justify-content-center align-items-center my-5 my-xl-0 my-lg-0 my-md-none">
+                        <div className="card h-20 ">
+                            <img
+                                className="bg-transparent"
+                                src={elem.image_url}
+                            />
+                        </div>
                     </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img
-                            src={mexc_img}
-                            className="d-block w-100"
-                            alt="..."
-                            height="100"
-                            width="100"
-                        />
-                    </div>
-                </div>
-                <button
-                    className="carousel-control-prev"
-                    type="button"
-                    data-bs-target="#carouselExampleDark"
-                    data-bs-slide="prev"
-                >
-                    <span
-                        className="carousel-control-prev-icon"
-                        aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                    className="carousel-control-next"
-                    type="button"
-                    data-bs-target="#carouselExampleDark"
-                    data-bs-slide="next"
-                >
-                    <span
-                        className="carousel-control-next-icon"
-                        aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+                ))}
             </div>
         </div>
     );
 };
-
+// container d-flex justify-content-center
+// d-flex pt-3 pb-5 justify-content-center
 export default Backers;
---
+++ src/config/HomeConfig/BackersConfig/config.backers.js
@@ -0,0 +1,88 @@
+import twin_apex from '../../../assets/partners/twin_apex.png';
+import x21 from '../../../assets/partners/21.png';
+import m6 from '../../../assets/partners/M6.png';
+import exwhite from '../../../assets/partners/exwhite.png';
+import Atlasfund from '../../../assets/partners/Atlasfund.png';
+import blockcapital from '../../../assets/partners/blockcapital.png';
+import dweb from '../../../assets/partners/DWEB.png';
+import roundtable from '../../../assets/partners/roundtable.png';
+import maximus from '../../../assets/partners/maximus.png';
+import tokenova from '../../../assets/partners/tokenova.png';
+import kretos from '../../../assets/partners/kretos.png';
+import ccix from '../../../assets/partners/ccix.png';
+import wubba from '../../../assets/partners/wubba.png';
+import mintedlabs from '../../../assets/partners/mintedlabs.png';
+import iog from '../../../assets/partners/oig_grey.png';
+import viviancapital from '../../../assets/partners/viviancapital.png';
+import crazynft from '../../../assets/partners/crazynft.png';
+
+export const BACKERS_DATA = [
+    {
+        id: 1,
+        image_url: twin_apex,
+    },
+    {
+        id: 2,
+        image_url: x21,
+    },
+    {
+        id: 3,
+        image_url: m6,
+    },
+    {
+        id: 4,
+        image_url: exwhite,
+    },
+    {
+        id: 5,
+        image_url: Atlasfund,
+    },
+    {
+        id: 6,
+        image_url: blockcapital,
+    },
+    {
+        id: 7,
+        image_url: dweb,
+    },
+    {
+        id: 8,
+        image_url: roundtable,
+    },
+    {
+        id: 9,
+        image_url: maximus,
+    },
+    {
+        id: 10,
+        image_url: tokenova,
+    },
+    {
+        id: 11,
+        image_url: kretos,
+    },
+    {
+        id: 12,
+        image_url: ccix,
+    },
+    {
+        id: 13,
+        image_url: wubba,
+    },
+    // {
+    //     id: 14,
+    //     image_url: mintedlabs,
+    // },
+    {
+        id: 14,
+        image_url: iog,
+    },
+    {
+        id: 15,
+        image_url: viviancapital,
+    },
+    {
+        id: 16,
+        image_url: crazynft,
+    },
+];
