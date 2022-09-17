f99faf597857eae926e0916f579a1463b7c9c5e2
Anish <anishtiwari12345@gmail.com>
2022-09-17 20:30:02 +0530

    fix

--- src/components/home/Backers/backers.js
+++ src/components/home/Backers/backers.js
@@ -12,14 +12,27 @@ const Backers = () => {
                 </h2>
             </div>
 
-            <div className=" row mx-0 ">
-                {BACKERS_DATA.map((elem) => (
-                    <div className="col-6 col-md-3 d-flex justify-content-center align-items-center my-4 my-xl-0 my-lg-0 my-md-none">
+            <div className=" row row-cols-2 row-cols-lg-5 row-cols-md-5  mx-0 ">
+                {BACKERS_DATA.map((elem, index) => (
+                    <div
+                        key={index}
+                        className="col-xs-6 d-flex justify-content-center align-items-center my-4 my-xl-0 my-lg-0 my-md-none"
+                    >
                         <div className="card h-20 ">
-                            <img
-                                className="bg-transparent"
-                                src={elem.image_url}
-                            />
+                            {index == 14 || index == 15 || index == 16 ? (
+                                <img
+                                    style={{
+                                        width: '150px',
+                                    }}
+                                    className="bg-transparent"
+                                    src={elem.image_url}
+                                />
+                            ) : (
+                                <img
+                                    className="bg-transparent"
+                                    src={elem.image_url}
+                                />
+                            )}
                         </div>
                     </div>
                 ))}
