997cb7dc01e72c5d9e03901f581ff1ea7b7d25a4
Ketan <krotusppp@gmail.com>
2022-09-27 17:56:36 +0530

    feat: added infinite scroll on backers

--- src/components/home/Backers/backers.js
+++ src/components/home/Backers/backers.js
@@ -1,126 +1,66 @@
 import React from "react";
 import { BACKERS_DATA } from "../../../config/HomeConfig/BackersConfig/config.backers";
-
-// const lerp = (current, target, factor) =>
-//   current * (1 - factor) + target * factor;
-
-// class LoopingText {
-//   constructor(el) {
-//     this.el = el;
-//     this.lerp = { current: 0, target: 0 };
-//     this.interpolationFactor = 0.1;
-//     this.speed = 0.2;
-//     this.events();
-//     this.render();
-//   }
-
-//   events() {
-//     window.addEventListener(
-//       "scroll",
-//       () => (this.lerp.target += this.speed * 5)
-//     );
-//   }
-
-//   animate() {
-//     this.lerp.target += this.speed;
-//     this.lerp.current = lerp(
-//       this.lerp.current,
-//       this.lerp.target,
-//       this.interpolationFactor
-//     );
-
-//     if (this.lerp.target > 100) {
-//       this.lerp.current -= this.lerp.target;
-//       this.lerp.target = 0;
-//     }
-
-//     this.el.style.transform = `translateX(${this.lerp.current}%)`;
-//   }
-
-//   render() {
-//     this.animate();
-//     window.requestAnimationFrame(() => this.render());
-//   }
-// }
-
 const Backers = () => {
-  const element = React.useRef();
-  let start, previousTimeStamp;
-  let done = false;
+  // const element = React.useRef();
+  // let start, previousTimeStamp;
+  // let done = false;
 
-  function step(timestamp) {
-    if (start === undefined) {
-      start = timestamp;
-    }
-    const elapsed = timestamp - start;
+  // function step(timestamp) {
+  //   if (start === undefined) {
+  //     start = timestamp;
+  //   }
+  //   const elapsed = timestamp - start;
 
-    if (previousTimeStamp !== timestamp) {
-      // Math.min() is used here to make sure the element stops at exactly 200px
-      const count = Math.min(0.1 * elapsed, 200);
-      element.current.style.transform = `translateX(${count}px)`;
-      if (count === 200) done = true;
-    }
+  //   if (previousTimeStamp !== timestamp) {
+  //     // Math.min() is used here to make sure the element stops at exactly 200px
+  //     const count = Math.min(0.1 * elapsed, 200);
+  //     element.current.style.transform = `translateX(${count}px)`;
+  //     if (count === 200) done = true;
+  //   }
 
-    if (elapsed < 2000) {
-      // Stop the animation after 2 seconds
-      previousTimeStamp = timestamp;
-      if (!done) {
-        window.requestAnimationFrame(step);
-      }
-    }
-  }
-  window.requestAnimationFrame(step);
+  //   if (elapsed < 2000) {
+  //     // Stop the animation after 2 seconds
+  //     previousTimeStamp = timestamp;
+  //     if (!done) {
+  //       window.requestAnimationFrame(step);
+  //     }
+  //   }
+  // }
+  // window.requestAnimationFrame(step);
   return (
     <div className="container px-5">
-      <div className="d-flex py-4 justify-content-center">
+      <div className="d-flex py-4 justify-content-center mb-4">
         <h2 className="fs-1 text-aileron-bold text-insta-regular">&nbsp;Our</h2>
         <h2 className="fs-1 text-aileron-bold services-text">&nbsp;Backers</h2>
       </div>
-      <div
-        style={{
-          display: "flex",
-          overflowX: "scroll",
-          marginTop: "20px",
-          transition: "ease 2s all",
-        }}
-        ref={element}
-      >
-        {BACKERS_DATA.map((item, index) => {
-          return (
-            <div
-              className="flex-shrink-0 mx-2"
-              style={{
-                width: "250px",
-                overflowX: "scroll",
-                height: "250px",
-                border: "6px solid red",
-              }}
-            >
-              {index + 1}
-            </div>
-          );
-        })}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          overflowX: "scroll",
-          marginTop: "60px",
-        }}
-      >
-        {BACKERS_DATA.map(() => {
-          return (
-            <div
-              className="flex-shrink-0"
-              style={{
-                width: "250px",
-                height: "250px",
-                border: "6px solid red",
-              }}
-            ></div>
-          );
-        })}
-      </div>
+      {[1, 2].map((item, index) => (
+        <div className="backers-scroll" key={index}>
+          <div className="backers_content">
+            {[
+              ...BACKERS_DATA,
+              ...BACKERS_DATA,
+              ...BACKERS_DATA,
+              ...BACKERS_DATA,
+              ...BACKERS_DATA,
+              ...BACKERS_DATA,
+            ].map((item, index) => {
+              return (
+                <div className="flex-shrink-0 mx-2 d-flex align-items-center justify-content-center">
+                  <div
+                    className="card h-20 my-4 card-img-top"
+                    style={{
+                      width: "150px",
+                      marginLeft: "50px",
+                    }}
+                  >
+                    <img src={item.image_url} alt={item.id} />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
