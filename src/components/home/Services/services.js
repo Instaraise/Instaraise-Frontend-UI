4179d9a1296c1ea4154f8f5a48554e33543b71d8
Ketan <krotusppp@gmail.com>
2022-09-29 14:04:58 +0530

    feat: add jsx/redundant text rule

--- src/components/home/Services/services.js
+++ src/components/home/Services/services.js
@@ -24,7 +24,7 @@ const Services = () => {
                                         <img
                                             className='ml-md-3'
                                             src={elem.image}
-                                            alt='Card image cap'
+                                            alt='Card_cap'
                                             width='50'
                                             height='52'
                                         />
@@ -45,7 +45,7 @@ const Services = () => {
                                         <img
                                             className='ml-md-3'
                                             src={elem.image}
-                                            alt='Card image cap'
+                                            alt='Card_cap'
                                             width='50'
                                             height='52'
                                         />
