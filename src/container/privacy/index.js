771cfec1220b193de85155acdd57bdf672699de1
Anish Tiwari <anish.at.tiwari@gmail.com>
2022-09-30 15:29:33 +0530

    fix: folder names caps to small

---
+++ src/container/privacy/index.js
@@ -0,0 +1,341 @@
+import React from 'react';
+
+import Footer from '../../components/Footer';
+import Header from '../../components/LandingPage/Header';
+const Privacy = () => {
+    React.useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }, []);
+    return (
+        <div>
+            <div className='header_nav_routes'>
+                <Header except={true} />
+            </div>
+            <div className='container text-dark-to-light terms_and_condition w-100 py-4 landing-page-container'>
+                <div className='terms'>
+                    <h3 className='title mb-5 fs-4'>Privacy Policy</h3>
+                    <p className='description font-insta-regular'>
+                        Herein is provided the privacy policy of Instaraise
+                        (owner and operator of Instaraise.io) This Privacy
+                        Policy describes how Instaraise.io will manage end
+                        user&apos;s personal information which we collect as a
+                        result of your use of Instaraise.io, your use of our
+                        websites located at www.Instaraise.io and any pages or
+                        websites under the &apos;Instaraise&apos; brand
+                        including any tablet or mobile phone applications that
+                        are owned and/or operated by us or whenever we otherwise
+                        deal with you. Instaraise is committed to ensuring that
+                        the personal information we collect about you is
+                        protected and is used, stored and disclosed in
+                        accordance with the Privacy Act and this Privacy Policy.
+                    </p>
+                    <br />
+                    <h2 className='description' style={{ fontSize: '20px' }}>
+                        Website use
+                    </h2>
+                    <br />
+                    <p className='description'>
+                        In using the website, you agree to be bound by the terms
+                        of this Privacy Policy. Instaraise.io may review the
+                        Privacy Policy periodically and reserves the right to
+                        change the Privacy Policy at any time at its discretion
+                        by posting such changes on the website. Changes will be
+                        effective immediately when posted on the website. The
+                        end users&apos; continued use of the website following
+                        posting will constitute your acceptance of those
+                        changes.
+                        <br />
+                        <br />
+                        <br />
+                        This Privacy Policy will not apply to websites which are
+                        linked to the website and over which Instaraise.io has
+                        no control.
+                        <br />
+                        <br />
+                        <br />
+                        Personal Information
+                        <br />
+                        <br />
+                        <br />
+                        The kinds of Personal Information that Instaraise.io may
+                        collect from you is:
+                        <br />
+                        <br />
+                        Name
+                        <br />
+                        <br />
+                        Email address
+                        <br />
+                        <br />
+                        Personally submitted preferences
+                        <br />
+                        <br />
+                        Location data
+                        <br />
+                        <br />
+                        IP address
+                        <br />
+                        <br />
+                        Collection Purposes
+                        <br />
+                        <br />
+                        <br />
+                        Instaraise.io will collect your Personal Information
+                        only by lawful and fair means and not in an intrusive
+                        way to operate its Service, and to provide you with the
+                        following information:
+                        <br />
+                        <br />
+                        If you wish to subscribe to Instaraise.io&apos;s weekly
+                        newsletter or other marketing communications.
+                        <br />
+                        <br />
+                        To update you on technical progress of Instaraise.
+                        <br />
+                        <br />
+                        To provide services to you as a User.
+                        <br />
+                        <br />
+                        To identify you as a User for security purposes and to
+                        comply with our legal obligations.
+                        <br />
+                        <br />
+                        To maintain your User account.
+                        <br />
+                        <br />
+                        To upgrade and enhance your experience within the
+                        website or over the telephone, or to tailor or develop
+                        information, services or products to suit your needs
+                        which may include market research and conducting
+                        promotions.
+                        <br />
+                        <br />
+                        To tell you about our products or services that we think
+                        may be of interest to you by communicating with you via
+                        email, SMS or telephone.
+                        <br />
+                        <br />
+                        To create aggregate data about Users through demographic
+                        profiling, statistical analysis of the database to
+                        provide to potential and existing Instaraise holders,
+                        and to allow for more efficient operation of
+                        Instaraise.io’s business.
+                        <br />
+                        <br />
+                        To comply with Instaraise.io’s legal and statutory
+                        obligations.
+                        <br />
+                        <br />
+                        For taking appropriate action if Instaraise.io has
+                        reason to suspect that unlawful activity or misconduct
+                        of a serious nature has been, is being or may be engaged
+                        in that relates to our functions and activities.
+                        <br />
+                        <br />
+                        To establish, exercise or defend any legal claims.
+                        <br />
+                        <br />
+                        Manage job applications.
+                        <br />
+                        <br />
+                        You may choose to deal with us on an anonymous basis or
+                        to use a pseudonym unless it is not practicable for us
+                        to deal with individuals who we cannot identify or we
+                        are required by law to only deal with identified
+                        individuals. Also, if you do not provide Instaraise.io
+                        with the Personal Information we request, we may be
+                        unable to process your request to become a User, provide
+                        you with our services or respond to your enquiry or
+                        complaint. Examples of Personal Information that may be
+                        collected by Instaraise.io include your name, email
+                        address, telephone numbers, your date of birth, other
+                        verification information such as your Service usage
+                        activities. By becoming a User or otherwise choosing to
+                        provide Instaraise.io with Personal Information you
+                        consent to Instaraise.io collecting, using and
+                        disclosing your Personal Information for the above
+                        purposes.
+                    </p>
+                    <p className='description'>
+                        <br />
+                        <br />
+                        <h3 style={{ fontSize: '20px' }}>
+                            Direct Marketing and Opting Out
+                        </h3>
+                        <br />
+                        <br />
+                        <br />
+                        Occasionally we may use your Personal Information to
+                        inform you about Instaraise.io&apos;s products or
+                        services or about promotional activities which
+                        Instaraise.io believes may be of interest or of benefit
+                        to you. We may do this via email, SMS, telephone or
+                        mail. If you no longer wish to receive marketing or
+                        promotional material from Instaraise.io at all or in any
+                        particular form, you may contact us at any time by email
+                        to{' '}
+                        <a
+                            href='mailto: connect@instaraise.io'
+                            className='email-link'
+                        >
+                            connect@instaraise.io
+                        </a>{' '}
+                        with your request with which we will comply as soon as
+                        is practical.
+                        <br />
+                        <br />
+                        From time to time we may contact you in relation to the
+                        management and administration of your Instaraise.io
+                        account. These communications can be via any of the
+                        modes of contact recorded when registering as a User.
+                        Such communication does not affect your opt-in or
+                        opt-out status for direct marketing communications.
+                        <br />
+                        <br />
+                        Management of your Personal Information
+                        <br />
+                        <br />
+                        <br />
+                        Instaraise.io will take all reasonable steps to ensure
+                        that the Personal Information which it collects, uses or
+                        discloses is correct and is stored in a secure
+                        environment which is accessed only by authorised
+                        persons.
+                        <br />
+                        <br />
+                        Instaraise.io will destroy or permanently de-identify
+                        the Personal Information we hold when it is no longer
+                        required for any purpose permitted under the APPs
+                        including our legal or operational obligations.
+                        <br />
+                        <br />
+                        Security of Personal Information
+                        <br />
+                        <br />
+                        <br />
+                        You acknowledge that no data transmission over the
+                        Internet is totally secure. Accordingly, Instaraise.io
+                        does not warrant the security of any information which
+                        you transmit to it. Any information which you transmit
+                        to Instaraise.io is transmitted at your own risk.
+                        However, once Instaraise.io receives your transmission,
+                        Instaraise.io will take reasonable steps to protect your
+                        Personal Information from misuse, loss and unauthorised
+                        access, modification and disclosure including by using
+                        password protected systems and databases and Secure
+                        Socket Layer (SSL) technology.
+                        <br />
+                        <br />
+                        Instaraise.io’s employees, agents and contractors are
+                        required to maintain the confidentiality of Users’
+                        Personal Information and trading behaviour.
+                        <br />
+                        <br />
+                        Information posted on bulletin boards or communicated
+                        within a social media environment (for example,
+                        Facebook, Twitter, Chat Rooms) becomes public
+                        information. Instaraise.io cannot guarantee the security
+                        of this type of disclosed information.
+                        <br />
+                        <br />
+                        We take seriously the responsibility to exclude children
+                        from access to our services. We will not accept their
+                        information for the purposes of accessing or using the
+                        Service. It is however, ultimately the responsibility of
+                        parents or guardians to monitor their children’s
+                        Internet activities including where appropriate by using
+                        Internet screening software.
+                        <br />
+                        <br />
+                        Remember to always log out of your account when you have
+                        completed your time on the website. This is particularly
+                        important if you share a computer with another person.
+                        You are responsible for the security of and access to
+                        your own computer, mobile device or any other handset
+                        used to access the website.
+                        <br />
+                        <br />
+                        Ultimately, you are solely responsible for maintaining
+                        the secrecy of your username, password and any account
+                        information. Please be careful whenever using the
+                        Internet and our website.
+                        <br />
+                        <br />
+                        Access to Personal Information
+                        <br />
+                        <br />
+                        <br />
+                        You may access the Personal Information collected by
+                        Instaraise.io by contacting us at{' '}
+                        <a
+                            href='mailto: connect@instaraise.io'
+                            className='email-link'
+                        >
+                            connect@instaraise.io
+                        </a>{' '}
+                        We will give you access to your Personal Information in
+                        the manner requested if that is possible and within a
+                        reasonable period. If we refuse your request or cannot
+                        give you access in the manner you have requested, we
+                        will do what we can to meet your requirements by other
+                        means. We may not be required to give you access to your
+                        Personal Information in certain circumstances which are
+                        set out in the APPs including where it may have an
+                        unreasonable impact on other individual&apos;s privacy.
+                        If we refuse access for such reasons, we will advise you
+                        in writing of the refusal and our reasons and the
+                        complaint mechanisms available to you.
+                        <br />
+                        <br />
+                        Use of Website
+                        <br />
+                        <br />
+                        <br />
+                        By using this website, you agree to Instaraise.io
+                        collecting and processing your Personal Information on
+                        the basis explained in this Privacy Policy.
+                        <br />
+                        <br />
+                        <br />
+                        Contact Details
+                        <br />
+                        <br />
+                        <br />
+                        If you have any queries, requests for access or
+                        correction or complaints relating to the handling of
+                        your personal information, please contact us by email.
+                        <br />
+                        <br />
+                        <br />
+                        Email:{' '}
+                        <a
+                            href='mailto: connect@instaraise.io'
+                            className='email-link'
+                        >
+                            connect@instaraise.io
+                        </a>
+                        <br />
+                        <br />
+                        <br />
+                        Updates to this Privacy Policy
+                        <br />
+                        <br />
+                        <br />
+                        Instaraise.io may review, change and update this Privacy
+                        Policy to reflect our current practices and obligations.
+                        We will publish our current Privacy Policy on our
+                        website at Instaraise.io and the changes will take
+                        effect at the time of publishing. You should review this
+                        privacy policy regularly and remain familiar with its
+                        terms.
+                    </p>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+export default Privacy;
