4932ec1f1258f3dbf452ef6c32965dd3a6106423
Anish Tiwari <anish.at.tiwari@gmail.com>
2022-10-03 20:08:41 +0530

    feat: added dark mode

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
+export const ThemeContext = React.createContext();
 
 import '../style/index.scss';
 
 import HomeContainer from '../container/home';
 import Privacy from '../container/privacy';
 import Terms from '../container/terms';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const Root = () => {
+    const [theme, setTheme] = useLocalStorage();
+    const themeclass = theme ? 'light' : 'dark';
+    const handleThemeChange = () => {
+        setTheme();
+    };
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<HomeContainer />} />
-                <Route path='/terms' element={<Terms />} />
-                <Route path='/privacy' element={<Privacy />} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeContext.Provider
+            value={{ theme, handleThemeChange: handleThemeChange }}
+        >
+            <div className={themeclass}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<HomeContainer />} />
+                        <Route path='/terms' element={<Terms />} />
+                        <Route path='/privacy' element={<Privacy />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </ThemeContext.Provider>
     );
 };
 
