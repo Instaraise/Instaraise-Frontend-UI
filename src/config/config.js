4c8400084b12ee87f4591d97901b68b35ccb0558
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-06-08 17:28:40 +0530

    chore: added new API and Refactored API code for improved readability

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,16 @@
 import { CONTRACT_CONFIG, testnetNetwork } from './network.config';
-export const COINGECKO_API_URL =
-    'https://2i1sxihk0h.execute-api.us-east-2.amazonaws.com/v1/market_chart';
 export const WALLET_NETWORK = 'mainnet';
 export const NAME = 'Instaraise';
 export const DEX_NETWORK = testnetNetwork;
-export const LIQUIDITY_DATA_URL =
-    'https://lieyn21sp6.execute-api.us-east-2.amazonaws.com';
+export const DATA_URL =
+    'https://ssuv8jnind.execute-api.us-east-2.amazonaws.com';
 export const DEX_FEE = 1;
-export const TOKEN_DATA_URL =
-    'https://01yilewb14.execute-api.us-east-2.amazonaws.com';
 export const DEX_DATA_REFRESH_TIME = 60000;
 export const NETWORK = 'mainnet';
 export const RPC_NODES = {
     testnet: CONTRACT_CONFIG[testnetNetwork].RPC,
     mainnet: 'https://mainnet.smartpy.io',
 };
-export const WHITELISTING_API_URL =
-    'https://okszprsix3.execute-api.us-east-2.amazonaws.com/v1/launchpad?';
-export const ENROLL_USER_API_URL = `https://a6m9yseuzl.execute-api.us-east-2.amazonaws.com`;
 export const TZKT_NODES = {
     mainnet: 'https://api.tzkt.io',
     testnet: CONTRACT_CONFIG[testnetNetwork].TZKT_NODE,
